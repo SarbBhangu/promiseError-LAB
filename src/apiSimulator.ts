e9223f83ddaeb6ecd44ce94bb010464b40ce79b6
SarbBhangu <Bhangu513@gmail.com>
2025-11-17 13:14:48 -0500

    added fetchProductReviews

--- src/apiSimulator.ts
+++ src/apiSimulator.ts
@@ -1,15 +1,31 @@
 export const fetchProductCatalog = (): Promise<{ id: number; name: string; price: number }[]> => {
-    return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if (Math.random() < 0.8) {
-        resolve([
+        return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() < 0.8) {
+            resolve([
             { id: 1, name: "Laptop", price: 1200 },
             { id: 2, name: "Headphones", price: 200 },
-        ]);
-        } else {
-        reject("Failed to fetch product catalog");
-        }
-    }, 1000);
+            ]);
+            }else {
+            reject("Failed to fetch product catalog");
+            }
+        }, 1000);
     });
 };
 
+export const fetchProductReviews = (productId: number): Promise<{ productId: number; rating: number; comment: string }[]> => {
+        return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = Math.random() < 0.8;
+
+            if (success) {
+            resolve([
+            { productId, rating: 4.5, comment: "Great product!" },
+            { productId, rating: 4.0, comment: "Pretty good overall." },
+            ]);
+            } else {
+            reject(`Failed to fetch reviews for product ID ${productId}`);
+            }
+        }, 1500);
+  });
+};
