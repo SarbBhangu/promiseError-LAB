287071a761346f67354047c84fae0606b769ac28
SarbBhangu <Bhangu513@gmail.com>
2025-11-17 14:32:55 -0500

    sales report added

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,39 @@ import {
   fetchSalesReport,
 } from "./apiSimulator";
 
+
 const runDashboard = () => {
   console.log("Starting dashboard...");
 
+  
   fetchProductCatalog()
     .then((products) => {
       console.log("Product catalog:");
       console.log(products);
 
       const reviewPromises = products.map((product) =>
-        fetchProductReviews(product.id).then((reviews) => ({
+      fetchProductReviews(product.id)
+        .then((reviews) => ({
           product,
           reviews,
         }))
         );
-        return Promise.all(reviewPromises)
+    return Promise.all(reviewPromises)
     })
     
     
     .then((productsWithReviews) => {
       console.log("Products with reviews:");
       console.log(productsWithReviews);
+
+      return fetchSalesReport().then((salesReport) => ({
+        productsWithReviews,
+        salesReport,
+      }));
+    })
+    .then((dashboardData) => {
+      console.log("Full dashboard data (products, reviews, sales):");
+      console.log(dashboardData);
     })
     .catch((error) => {
       console.error("Error while fetching product catalog:", error);
