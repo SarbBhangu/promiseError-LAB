c9c32c1af0741e5fef54c5ea8b84242856061798
SarbBhangu <Bhangu513@gmail.com>
2025-11-17 13:26:29 -0500

    added fetchSalesReport

--- src/apiSimulator.ts
+++ src/apiSimulator.ts
@@ -29,3 +29,22 @@ export const fetchProductReviews = (productId: number): Promise<{ productId: num
         }, 1500);
   });
 };
+
+export const fetchSalesReport = (): Promise<{totalSales: number; unitsSold: number; averagePrice: number;}> => {
+        return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = Math.random() < 0.8; 
+
+            if (success) {
+            resolve({
+            totalSales: 50000,
+            unitsSold: 120,
+            averagePrice: 416.67,
+            });
+            } else {
+            reject("Failed to fetch sales report");
+            }
+        }, 1000); 
+  });
+};
+
